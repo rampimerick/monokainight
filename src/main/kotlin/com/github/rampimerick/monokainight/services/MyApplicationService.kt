17ad6b6feb79ab1aee31ffa4684d4f1cd41e39d2
GitHub Action <action@github.com>
2023-01-24 16:34:49 +0000

    Template cleanup

--- src/main/kotlin/com/github/rampimerick/monokainight/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/rampimerick/monokainight/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.rampimerick.monokainight.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.rampimerick.monokainight.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/rampimerick/monokainight/services/MyProjectService.kt
+++ src/main/kotlin/com/github/rampimerick/monokainight/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.rampimerick.monokainight.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.rampimerick.monokainight.MyBundle
 
 class MyProjectService(project: Project) {
 
